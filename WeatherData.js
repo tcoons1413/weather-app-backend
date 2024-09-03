c2a6395838a5bf9664b202724343d35a521c438d
Thomas Coons <tomcoons1413@gmail.com>
2024-09-03 19:33:39 -0400

    Changes Weather API and updated backend

---
+++ WeatherData.js
@@ -0,0 +1,10 @@
+class WeatherData{
+    constructor(city, feelsLike, temp, hours){
+        this.city = city;
+        this.feelsLike = feelsLike;
+        this.temp = temp;
+        this.hours = hours;
+    }
+}
+
+module.exports=WeatherData
--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 "use strict";
 const express = require("express");
 require("dotenv").config();
-
+const cors = require("cors");
+const WeatherData = require("./WeatherData");
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(cors())
+
+
 
 app.get("/",(req,res)=>{
-    console.log("HELLOOOOOO");
     res.json({"key":"TEST"});
 })
 
 app.get("/api/weather", async (req,res)=>{
-    console.log("HELLOOO AGAIN!!!!!!")
+    const API_KEY = process.env.API_KEY;  
+
     try{
-        const city = req.query.city;
-        const url = "";
-        const API_KEY = process.env.API_KEY;
-        const response = await fetch(`${url}${city}`);
-        const data = await response.json();
-        res.json(data);
+        const city = req.query.name;
+
+        const responseCurrent = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`);
+        const responseForecast = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}`);
+
+        const current = await responseCurrent.json();
+        const forecast = await responseForecast.json();
+
+        const hours = forecast.forecast.forecastday[0].hour.map((hour)=>{
+            return {hour:hour.time.split(" ")[1],hourTemp:hour.temp_c}
+        })
+
+        const weatherData = new WeatherData(current.location.name, current.current.feelslike_c,current.current.temp_c,hours)
+
+        res.json(weatherData);
     }
     catch(err){
-        res.status(500).json(err);
+        res.status(500).json(err.message);
     }
 })
 
